ce6dc827f6ca6bacb3a68fe9ab8472bedf250456
Talari Praveen Kumar <116370782+code-with-praveen143@users.noreply.github.com>
2025-01-02 13:04:51 +0530

    Update page.tsx

--- Client/src/app/dashboard/payment-gateway/page.tsx
+++ Client/src/app/dashboard/payment-gateway/page.tsx
@@ -16,7 +16,7 @@ export default function EncryptURL() {
       returnURL: "https://khit.campusify.io/dashboard/return-url",
       referenceNo: "123456",
       subMerchantID: "11",
-      transactionAmount: "100",
+      transactionAmount: "1",
       payMode: "9",
     };
 
