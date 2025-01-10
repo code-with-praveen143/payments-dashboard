836ce792b98423e2985f98c66c7272422d4e0702
Talari Praveen Kumar <116370782+code-with-praveen143@users.noreply.github.com>
2025-01-10 17:12:54 +0530

    Update encryptionController.js

--- server/controllers/encryptionController.js
+++ server/controllers/encryptionController.js
@@ -42,7 +42,7 @@ exports.createEncryptedURL = async (req, res) => {
         merchantID,
         mandatoryFields,
         optionalFields: optionalFields,
-        returnURL: "http://localhost:3000/dashboard/return-url",
+        returnURL: "https://khit.campusify.io/dashboard/return-url",
         referenceNo,
         subMerchantID,
         transactionAmount,
